846a6143da6e8c36a70c3b11992827a0bb50e221
rodmoioliveira <rodolfo.oliveira@tvglobo.com.br>
2020-07-16 20:20:09 -0300

    altera estilos

--- src/cljs/pokemon/components.cljs
+++ src/cljs/pokemon/components.cljs
@@ -7,8 +7,7 @@
 (defn poke-store-type
   [poketype]
   [:a.poketype-link
-   {:style {:background-color (str "var(--" poketype ")")}
-    :href (path-for (-> poketype keyword))
+   {:href (path-for (-> poketype keyword))
     :key poketype
     :on-click (set-theme! poketype)}
    [:img.poketype-img {:src (-> poketype keyword poketypes-info :src)}]
