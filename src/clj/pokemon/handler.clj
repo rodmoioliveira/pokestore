13ac0657d154b3d3a311bdbfbbd620406e9ad416
rodmoioliveira <rodolfo.oliveira@tvglobo.com.br>
2020-07-19 21:37:07 -0300

    muda fonte para lato

--- src/clj/pokemon/handler.clj
+++ src/clj/pokemon/handler.clj
@@ -18,6 +18,8 @@
    [:meta {:charset "utf-8"}]
    [:meta {:name "viewport"
            :content "width=device-width, initial-scale=1"}]
+   [:link {:href "https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap"
+           :rel "stylesheet"}]
    (include-css (if (env :dev) "/css/site.css" "/css/site.min.css"))])
 
 (defn loading-page []
